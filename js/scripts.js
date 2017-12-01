ce57fc227cd3f436bfe802c13ab26f1d5c6e1159
Rodnier Borrego <rodnierbc89@gmail.com>
2017-12-01 14:25:04 -0800

    include the paragraphs with descriptions of the programming language

--- js/scripts.js
+++ js/scripts.js
@@ -19,6 +19,23 @@ $(document).ready(function() {
     var developer = $("input:radio[name=developer]:checked").val();
     var mathSkills = $("input:radio[name=mathSkills]:checked").val();
 
+    if((math === "2" || math === "1") && mathSkills === "1" && developer ==="1"){
+      $("#actors").show();
+      $("#angelinaJ").hide();
+      $("#leonardoD").show();
+    }
+    else if(curious === "1" && developer === "0"){
+      //$(".animalName-title").text(animal);
+      $("#actors").show();
+      $("#leonardoD").hide();
+      $("#angelinaJ").show();
+    }
+    else if(!(math === "0") && developer ==="1" && mathSkills===0){
+      
+    }
+    else{
+
+    }
     event.preventDefault();
   });
 });
